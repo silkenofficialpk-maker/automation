4e606a6e064858ed5674fc3daac5106361c535b7
silkenofficialpk-maker <silkenofficialpk@gmail.com>
2025-08-30 15:35:33 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ import admin from "firebase-admin";
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 
+// fix private_key line breaks
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -555,6 +558,7 @@ app.get("/demo/send", async (req, res) => {
 
 /* ---------- Start server ---------- */
 app.listen(PORT, () => console.log(`⚡ Server running on port ${PORT}`));
+
 
 
 

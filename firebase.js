52a68ac98dc9276ff6cb46dd28e8cd93e34c8d91
silkenofficialpk-maker <silkenofficialpk@gmail.com>
2025-09-04 18:37:16 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,8 @@
-// firebase.js (Backend version)
-const admin = require("firebase-admin");
+var admin = require("firebase-admin");
 
-// Load service account key from environment variable (safe way)
-// JSON ko string ke form me store karke parse karna
-const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
+var serviceAccount = require("path/to/serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://automation-4b66d-default-rtdb.firebaseio.com"
 });
-
-const db = admin.database();
-
-module.exports = db;

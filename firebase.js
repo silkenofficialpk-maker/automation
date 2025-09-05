9e5db567a4fbf6a03a885a572638ca37cc41912a
silkenofficialpk-maker <silkenofficialpk@gmail.com>
2025-09-05 19:11:15 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,1 @@
-var admin = require("firebase-admin");
 
-var serviceAccount = require("path/to/serviceAccountKey.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://automation-4b66d-default-rtdb.firebaseio.com"
-});

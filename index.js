dea4b0e4a9b6ac869eaa9d86475080e587240b20
silkenofficialpk-maker <silkenofficialpk@gmail.com>
2025-09-16 05:59:02 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1142,7 +1142,9 @@ app.get("/", (req, res) => {
 });
 // ---------------- Start Server ----------------
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`⚡ Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`⚡ Server running on port ${PORT}`);
+});
 
 
 

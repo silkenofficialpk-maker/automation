cfb764ab9c87c6c37d576af20ada91d374420f4e
silkenofficialpk-maker <silkenofficialpk@gmail.com>
2025-09-16 06:46:33 +0500

    Update index.js

--- index.js
+++ index.js
@@ -34,8 +34,7 @@ admin
     console.error("❌ Service account failed:", err.message);
   });
 
-const db = admin.database();
-console.log("✅ Realtime DB connected");
+
 
 // ---- Express Setup ----
 const app = express();
@@ -1134,6 +1133,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`⚡ Server running on port ${PORT}`);
 });
+
 
 
 
